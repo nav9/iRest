92bf4dcb82e6c5209e02e4710bf48df16f62c098
Navin <navinipe@gmail.com>
2022-05-07 14:07:23 +0530

    File handler full text now written to file

--- timer/timers.py
+++ timer/timers.py
@@ -6,6 +6,7 @@ logFileName = 'logs.log' #TODO: Shift to config file
 logging.basicConfig(level=logging.INFO, format='%(levelname)s %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 log = logging.getLogger(__name__)
 handler = RotatingFileHandler(logFileName, maxBytes=2000, backupCount=5)#TODO: Shift to config file
+handler.formatter = logging.Formatter(fmt='%(levelname)s %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S') #setting this was necessary to get it to write the format to file. Without this, only the message part of the log would get written to the file
 log.addHandler(handler)
 log.setLevel(logging.INFO)
 

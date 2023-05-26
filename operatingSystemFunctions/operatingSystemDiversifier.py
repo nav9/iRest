1a98ee4595a9ff3d3d13541b0a33b59f75b98fbd
Navin <navinipe@gmail.com>
2023-05-26 18:15:31 +0530

    screen lock handler working perfectly

--- operatingSystemFunctions/operatingSystemDiversifier.py
+++ operatingSystemFunctions/operatingSystemDiversifier.py
@@ -1,16 +1,10 @@
 import logging
 import subprocess
-import PySimpleGUI
-import sys
 from sys import platform #to check which OS the program is running on
 from abc import ABC, abstractmethod
 from operatingSystemFunctions import audioNotifiers
 from operatingSystemFunctions import graphicalNotifiers
 from operatingSystemFunctions import screenLockCheckers
-
-class Constants:
-    FIRST_ELEMENT_OF_ARRAY = 0
-    SECOND_ELEMENT_OF_ARRAY = 1
 
 class OperatingSystemFunctionality(ABC):#Abstract parent class
     #Note: Abstract methods have to be implemented by child classes because they would be invoked by other classes
@@ -24,17 +18,22 @@ class OperatingSystemFunctionality(ABC):#Abstract parent class
         """ Returns a reference to the audio notification instance created for a particular operating system """
         pass    
 
+    @abstractmethod
+    def getGraphicalNotifier(self):
+        """ Returns a reference to the graphical notification instance created for a particular operating system """
+        pass 
+
 class LinuxFunctionality(OperatingSystemFunctionality):#For functions that are specific to Linux. The program uses this class only if it detects it is running on Linux. These same functions should also be available in a "Windows" class and that class would be instantiatiated and used if the program is run on Windows
     #cat /etc/os-release prints the Linux flavour
     def __init__(self):  
         self.encoding = 'utf-8'
-        self.gnomeScreensaverPresent = self.__isGnomeScreensaverPresent()
         #TODO: code needs to be written to dynamically switch to any other notifier
         self.audioNotifier = audioNotifiers.SpeedSaySpeechNotifier_Linux()
         self.graphicalNotifier = graphicalNotifiers.PlyerGraphicalNotifier()
+        self.desktopAdapter = LinuxDesktopAdapter()
     
     def isScreenLocked(self):
-        return self.__isScreenLocked()
+        return self.desktopAdapter.isScreenLocked()
 
     def getAudioNotifier(self):
         return self.audioNotifier
@@ -42,73 +41,7 @@ class LinuxFunctionality(OperatingSystemFunctionality):#For functions that are s
     def getGraphicalNotifier(self):
         return self.graphicalNotifier
     
-    def __isScreenLocked(self):
-        #To check in Ubuntu, use: gnome-screensaver-command -q
-        #To check in Mint, use: cinnamon-screensaver-command -q
-        theProcess = subprocess.Popen('cinnamon-screensaver-command -q', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        locked = False
-        screenLocked = "is active"
-        screenNotLocked = "is inactive"
-        receivedOutput = " ".join(str(x) for x in theProcess.stdout.readlines()) #readlines() returns a list. This line converts the list to a string
-        if screenLocked in receivedOutput:
-            locked = True
-        else: 
-            if screenNotLocked in receivedOutput:
-                locked = False
-            else:
-                logging.error(f"GNOME SCREENSAVER OUTPUT UNKNOWN. CHECK AND REPROGRAM: {receivedOutput}")                            
-        logging.debug(f"SCREEN LOCKED status: {locked}")        
-        return locked
     
-    def __isThisAppInstalled(self, appName):
-        appInstalled = False
-        status = subprocess.getstatusoutput("dpkg-query -W -f='${Status}' " + appName)
-        if not status[Constants.FIRST_ELEMENT_OF_ARRAY]:
-            if 'installed' in status[Constants.SECOND_ELEMENT_OF_ARRAY]:
-                appInstalled = True
-        return appInstalled
-
-    def __isGnomeScreensaverPresent(self):
-        screenSaverPresent = False
-        #while not screenSaverPresent:
-        try:
-            app = 'gnome-screensaver'
-            logging.info(f"Checking if {app} is installed...")
-            screenSaverPresent = self.__isThisAppInstalled(app)
-            #response = subprocess.Popen(['gnome-screensaver'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            #stdout, stderr = response.communicate()
-            #if stderr != None:
-            #    logging.error("Error during check for Gnome screensaver: ", stderr)                                        
-            #stdoutConverted = stdout.decode(self.encoding)
-            #logging.info(f"stdout is {stdoutConverted}")
-            #logging.info(f"stderr is {stderr}")
-            #if 'not found' in stdoutConverted:                     
-            #    screenSaverPresent = False
-            #if 'screensaver already running' in stdoutConverted: 
-            #    screenSaverPresent = True
-            #    logging.info("Gnome screensaver detected. Lock-screen detection will work fine.")
-        except FileNotFoundError as e:
-            screenSaverPresent = False
-            logging.error(f"Error encountered: {e}")
-        if not screenSaverPresent:
-            logging.info("--------- INSTALLATION REQUIRED ---------")                      
-            errorMessage = "Gnome screensaver is missing (needed for lock-screen detection). Please install it using 'sudo apt install -y gnome-screensaver'"
-            logging.info(errorMessage)
-            PySimpleGUI.popup(errorMessage, title="iRest")
-            #sys.exit()
-            #screensaverInstallCommands = ['sudo', 'apt', 'install', '-y', 'gnome-screensaver'] 
-            #logging.info("Running this command: ", ' '.join(screensaverInstallCommands))
-            #response = subprocess.Popen(screensaverInstallCommands, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)   
-            #response.wait()  
-            #logging.info("Installing...")           
-        logging.info(f"Screensaver present: {screenSaverPresent}")
-        return screenSaverPresent
-
-# class OperatingSystemID:#This might eventually need more elaborate ID codes based on flavours of Linux or versions of Windows etc.
-#     LINUX = 0
-#     WINDOWS = 1
-#     MAC = 2
-
 class OperatingSystemIdentifier:    
     def __init__(self):
         #self.currentOperatingSystem = None
@@ -149,7 +82,8 @@ class LinuxDesktops:#Note: all strings should be in lower case, since the deskto
 class LinuxDesktopAdapter:
     def __init__(self):
         self.screenLockChecker = None
-        desktopName = self.__getDesktopName()        
+        desktopName = self.__getDesktopName()    
+        #---assign handler based on the desktop detected    
         if LinuxDesktops.GNOME in desktopName:            
             self.screenLockChecker = screenLockCheckers.GnomeScreenLockCheck()            
         if LinuxDesktops.CINNAMON in desktopName:
--- operatingSystemFunctions/screenLockCheckers.py
+++ operatingSystemFunctions/screenLockCheckers.py
@@ -1,8 +1,12 @@
-import os
+import PySimpleGUI
 import logging
 import subprocess
 from abc import ABC, abstractmethod
 from configuration import configHandler
+
+class Constants:
+    FIRST_ELEMENT_OF_ARRAY = 0
+    SECOND_ELEMENT_OF_ARRAY = 1
 
 class ScreenLockChecker(ABC):#Abstract parent class
     #Note: Abstract methods have to be implemented by child classes because they would be invoked by other classes
@@ -20,6 +24,7 @@ class GnomeScreenLockCheck(ScreenLockChecker):
     def __init__(self):
         self.id = "Gnome Desktop screen lock checker"
         self.screensaverCommand = "gnome-screensaver-command -q" 
+        self.__isGnomeScreensaverPresent()
 
     def execute(self):
         theProcess = subprocess.Popen(self.screensaverCommand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -36,6 +41,32 @@ class GnomeScreenLockCheck(ScreenLockChecker):
                 logging.error(f"SCREENSAVER OUTPUT UNKNOWN. CHECK AND REPROGRAM: {receivedOutput}")                            
         logging.debug(f"SCREEN LOCKED status: {locked}")        
         return locked
+
+    def __isThisAppInstalled(self, appName):
+        appInstalled = False
+        status = subprocess.getstatusoutput("dpkg-query -W -f='${Status}' " + appName)
+        if not status[Constants.FIRST_ELEMENT_OF_ARRAY]:
+            if 'installed' in status[Constants.SECOND_ELEMENT_OF_ARRAY]:
+                appInstalled = True
+        return appInstalled
+
+    def __isGnomeScreensaverPresent(self):#Ubuntu does not have the screensaver installed by default
+        screenSaverPresent = False
+        try:
+            app = 'gnome-screensaver'
+            logging.info(f"Checking if {app} is installed...")
+            screenSaverPresent = self.__isThisAppInstalled(app)
+        except FileNotFoundError as e:
+            screenSaverPresent = False
+            logging.error(f"Error encountered: {e}")
+        if not screenSaverPresent:
+            logging.info("--------- INSTALLATION REQUIRED ---------")                      
+            errorMessage = "Gnome screensaver is missing (needed for lock-screen detection). Please install it using 'sudo apt install -y gnome-screensaver' and restart iRest."
+            logging.info(errorMessage)
+            PySimpleGUI.popup(errorMessage, title="iRest")            
+        logging.info(f"Screensaver present: {screenSaverPresent}")
+        return screenSaverPresent
+
 
 class CinnamonScreenLockCheck(ScreenLockChecker):
     def __init__(self):
